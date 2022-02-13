1ac0e25c949be113b4c12fdd745e286289520958
Aditya Pansari <iadityapansari@gmail.com>
2022-02-13 21:29:47 +0530

    Adds feature - show score and timer

--- src/App.js
+++ src/App.js
@@ -2,17 +2,25 @@ import React from "react";
 import Dice from "./components/Dice";
 import { nanoid } from "nanoid";
 import Confetti from "react-confetti";
+import TimeCounter from "./components/TimeCounter";
 
 function App() {
   const [dice, setDice] = React.useState(allNewDice());
   const [tenzies, setTenzies] = React.useState(false);
+  const [gameStarted, setGameStarted] = React.useState(false);
+  const [score, setScore] = React.useState({ best: 5, current: 0 });
 
   React.useEffect(() => {
+    if (!gameStarted && dice.some((d) => d.isHeld)) setGameStarted(true);
+
     const firstVal = dice[0].value;
     const allHeldAndSame = dice.every(
       (d) => !!d.isHeld && d.value === firstVal
     );
-    !!allHeldAndSame && setTenzies(true);
+    if (!!allHeldAndSame) {
+      setTenzies(true);
+      setGameStarted(false);
+    }
   }, [dice]);
 
   function generateNewDie() {
@@ -41,6 +49,9 @@ function App() {
     } else {
       setTenzies(false);
       setDice(allNewDice());
+      setScore((prevScore) => {
+        return { ...prevScore, current: 0 };
+      });
     }
   }
 
@@ -64,6 +75,11 @@ function App() {
         Roll until all dice are the same. Click each die to freeze it at its
         current value between rolls.
       </p>
+      <TimeCounter
+        gameStarted={gameStarted}
+        score={score}
+        setScore={setScore}
+      />
       <div className="dice-container">{diceElements}</div>
       <button className="btn" onClick={rollDice}>
         {!tenzies ? "Roll" : "New Game"}
---
+++ src/components/TimeCounter.js
@@ -0,0 +1,52 @@
+import React, { useEffect } from "react";
+
+export default function TimeCounter({ gameStarted, score, setScore }) {
+  const [sec, setTime] = React.useState(0);
+  let [timerId, setTimerId] = React.useState(null);
+
+  useEffect(() => {
+    console.log("useEffect", gameStarted);
+    if (gameStarted) {
+      console.log("started");
+      startCounter();
+    } else {
+      stopCounter();
+    }
+  }, [gameStarted]);
+
+  function startCounter() {
+    let timer = setInterval(() => {
+      setTime((prevTime) => ++prevTime);
+    }, 1000);
+    setTimerId(timer);
+  }
+
+  function stopCounter() {
+    if (!timerId) return;
+    console.log("timerId ", timerId);
+    setScore((prevScore) => {
+      let best = sec < prevScore.best ? sec : prevScore.best;
+      return { ...prevScore, current: sec, best: best };
+    });
+    setTime(0);
+    clearInterval(timerId);
+  }
+
+  return (
+    <div className="score-card">
+      <div className="score">
+        Best &#8594; <span>{score.best}</span>
+      </div>
+
+      {score.current ? (
+        <div className="score">
+          Score &#8594; <span>{score.current}</span>
+        </div>
+      ) : (
+        <div className="score">
+          Timer &#8594; <span>{sec}</span>
+        </div>
+      )}
+    </div>
+  );
+}
